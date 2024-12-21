8097566f6cf6e6c7a0881b6df211a65dcb9e8663
978sebasBeller <sebastian.beller@ucb.edu.bo>
2024-12-21 15:18:03 -0400

    Enviroments file was added

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    API_URL:import.meta.env.NG_APP_API_URL_PRODUCCION,
+    API_KEY_FIRE_BASE: import.meta.env.NG_APP_API_KEY_FIRE_BASE,
+    AUTH_DOMAIN: import.meta.env.NG_APP_AUTH_DOMAIN,
+    PROJECT_ID: import.meta.env.NG_APP_PROJECT_ID,
+    STORAGE_BUCKET: import.meta.env.NG_APP_STORAGE_BUCKET,
+    MESSAGING_SENDER: import.meta.env.NG_APP_MESSAGING_SENDER,
+    APP_ID: import.meta.env.NG_APP_APP_ID,
+    MEASUREMENT_ID: import.meta.env.NG_APP_MEASUREMENT_ID,
+    API_GROK:import.meta.env.NG_APP_API_GROK
+};
+
+
